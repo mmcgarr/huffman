1822a10fd3035eb41c31a9cbceeef6621a862a11
mmcgarr <mmcgarr.tcd.ie>
2013-05-25 10:15:04 +0100

    Adding more comments

--- ComplexNode.h
+++ ComplexNode.h
@@ -1,3 +1,7 @@
+// Class to represent nodes in the tree which solely point to other nodes. 
+// The position in the tree determines the character's encoding.
+// 'Left' get an encoding of '0' and 'Right' gets '1'.
+// Leaf Nodes are referred to as 'SimpleNodes' and these represent the individual characters
 #include "Node.h"
 
 class ComplexNode : public Node{
